07644e8c05611cf4d798a743c9fae6cb03970c4b
Sevenworks <91027492+SevenworksDev@users.noreply.github.com>
2023-02-01 11:13:43 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const Scratch = require("scratch-site-api")
+const fashion = require("fashion")
+var prompt = require('prompt-sync')();
 const fs = require("fs/promises")
 const client = new Scratch.User()
 const users = require("./users.json")
-const SCRATCHUSERNAME = ""
-const SCRATCHPASSWORD = ""
+
+console.log(fashion.magentaBright("Scratch Spambot - By: Sevenworks"))
+console.log(fashion.magentaBright("https://github.com/SevenworksDev/Scratch-Spambot"))
+console.log(fashion.magentaBright("______________________________________________________________"))
+
+const scratchusername = prompt(fashion.yellowBright("Scratch Username: "))
+const scratchpassword = prompt(fashion.yellowBright("Scratch Password: "))
 
 const messages = [
-    "SUB_SCR_IBE TO SEVENWORKS AT YouTube.com/@sevenworks !!!"
+    "SUB TO SEVENWORKS AT YouTube.com/@sevenworks !!!"
 ]
 
 const rekt = async () => {
@@ -18,10 +25,10 @@ const rekt = async () => {
 const comment = async (user) => {
     const message = messages[Math.floor(Math.random()*messages.length)]
     await client.comments.commentOnUser(message, user)
-    console.log(`Commented on ${user}`)
+    console.log(fashion.greenBright(`Commented on ${user}`))
     setTimeout(() => {
         comment(users[Math.floor(Math.random() * users.length)])
-    }, (3000) + (Math.random() * 2) * 3000)
+    }, (2000) + (Math.random() * 2) * 2000)
 }
 
 rekt()
